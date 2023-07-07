621e59eaba4578cdd7e8253cfb101c87a82102c5
CEGonzalezRojas <claudioesteban.gonzalezrojas@gmail.com>
2023-07-06 21:50:21 -0400

    Ajustes para mobiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,15 +36,12 @@ const shareCard = _ => {
 }
 
 export const dataURLtoFile = (dataurl, filename) => {
-    var arr = dataurl.split(","),
-        mimeType = arr[0].match(/:(.*?);/)[1],
-        decodedData = atob(arr[1]),
-        lengthOfDecodedData = decodedData.length,
-        u8array = new Uint8Array(lengthOfDecodedData);
-    while (lengthOfDecodedData--) {
-        u8array[lengthOfDecodedData] = decodedData.charCodeAt(lengthOfDecodedData);
+    var blobBin = atob(dataurl.split(',')[1]);
+    var array = [];
+    for(var i = 0; i < blobBin.length; i++) {
+        array.push(blobBin.charCodeAt(i));
     }
-    return new File([u8array], filename, { type: mimeType });
+    return new Blob([new Uint8Array(array)], {type: 'image/png'});
 };
 
 const shareFile = (file, title, text) => {
