5ea9d6c6c30f002bf9b109d2a665e2ef34ec2d8f
CEGonzalezRojas <claudioesteban.gonzalezrojas@gmail.com>
2023-07-06 21:41:38 -0400

    Pruebas mobiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,37 @@ function Fade(event){
     const humitaYear = document.querySelector(".humita-year");
     if(humitaYear) humitaYear.dataset.year =  new Date().getFullYear();
 })();
+
+// Compartir card
+const shareCard = _ => {
+    var card = document.querySelector("body");
+    html2canvas(card, {windowWidth: 540, windowHeight: 960, backgroundColor: null}).then((canvas) => {
+        //var link = document.createElement("a");
+        //document.body.appendChild(link);
+        shareFile(canvas.toDataURL(), "JanKenUP! Card");
+        
+        //link.download = "html_image.jpg";
+        //link.href = canvas.toDataURL();
+        //link.target = '_blank';
+        //link.click();
+    });
+}
+
+const shareFile = (file, title, text) => {
+    if (navigator.canShare && navigator.canShare({ files: [file] })) {
+        navigator
+        .share({
+            files: [file],
+            title,
+            text
+        })
+        .then(() => console.log("Share was successful."))
+        .catch((error) => console.log("Sharing failed", error));
+    } else {
+        console.log(`Your system doesn't support sharing files.`);
+    }
+};
+
+document.querySelector(".card").addEventListener("mouseup", _ => {
+    shareCard();
+})
